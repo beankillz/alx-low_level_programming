0c10feb3dd6d1b59255f267efbe8b9d8b24d48e9
beankillz <princechairmanivan@gmail.com>
2023-12-18 19:04:00 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * Using printf to print a string
- * main - Returns the string
+ * main - Entry point
  * Return - Returns 0 on success
  */
 int main(void)
-{
 /*using printf to print a string*/
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
 
