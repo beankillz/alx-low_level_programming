dda10cad19d0298a57593738e898f2418e2937e0
beankillz <princechairmanivan@gmail.com>
2023-12-19 08:14:16 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,12 @@
 /**
  * main - Entry point
  *
+ * Description: This is the main function of thi program.
+ *              It returns 0 on succesful execution.
+ *
  * Return - Always 0 (Success)
  */
-int main(0)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
