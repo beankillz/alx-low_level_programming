22b9ba8d9128a258d138dff4084057906c2adb45
beankillz <princechairmanivan@gmail.com>
2023-12-20 19:17:41 -0800

    Assembler with intel format

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -4,13 +4,8 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
-
  */
-int  main(void)
-{ 
-   const char *message = "Programming is like building a multilingual puzzle.";
-
-   puts(message);
-
-    return (0);
+int main(void)
+{
+return (0);
 }
