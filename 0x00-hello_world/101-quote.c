d7314ee7c67c80d88f8edc5f303535a11c61de8e
beankillz <princechairmanivan@gmail.com>
2023-12-21 21:06:20 -0800

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(1,quo,55);
+stderr;
+return (1);
+}
