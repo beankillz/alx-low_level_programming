2c4b04147fb2604580786220f35882fb73059255
beankillz <princechairmanivan@gmail.com>
2024-01-02 20:30:15 -0800

    Second commit on pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints the reverse of the oringinal string
+ * @s: string to be printed
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+		for (i = 0; s[i] != '\0'; i++)
+			count++;
+		for (i = count - 1; i >= 0; i--)
+			_putchar(s[i]);
+		_putchar('\n');
+}
